63941241f0aee60c6c447e22e7c1288434bb416b
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-10-30 11:13:54 +0100

    Added EventsClient class and model for Events

---
+++ BalticMarinasClient/ApiClient/EventsClient.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalticMarinasClient.ApiClient
+{
+    public class EventsClient
+    {
+        private readonly HttpClient _httpClient;
+        private Uri BaseEndpoint { get; set; }
+
+        public EventsClient(Uri baseEndpoint)
+        {
+            if (baseEndpoint == null)
+            {
+                throw new ArgumentNullException("baseEndpoint");
+            }
+            BaseEndpoint = baseEndpoint;
+            _httpClient = new HttpClient();
+        }
+
+        private async Task<T> GetAsync<T>(Uri requestUrl)
+        {
+
+            var response = await _httpClient.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        private static JsonSerializerSettings MicrosoftDateFormatSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                };
+            }
+        }
+
+        private HttpContent CreateHttpContent<T>(T content)
+        {
+            var json = JsonConvert.SerializeObject(content, MicrosoftDateFormatSettings);
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+
+    }
+}
---
+++ BalticMarinasClient/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BalticMarinasClient.Models
+{
+    public class Event
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public int Name { get; set; }
+
+        [JsonProperty("location")]
+        public int Location { get; set; }
+    }
+}
