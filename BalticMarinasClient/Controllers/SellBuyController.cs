68029883b3a2737751d365fef2fd689a30444268
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-03 20:56:58 +0100

    Added SellBuy Index view and connection to SellBuyWS

--- BalticMarinasClient/Controllers/SellBuyController.cs
+++ BalticMarinasClient/Controllers/SellBuyController.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BalticMarinasClient.ApiClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BalticMarinasClient.Controllers
 {
     public class SellBuyController : Controller
     {
+        SellBuyClient sellBuyClient = new SellBuyClient();
+
         public IActionResult Index()
         {
+            var items = sellBuyClient.GetAllSoldItems().Result;
+            ViewBag.ItemsList = items;
             return View();
         }
     }
