584601830eedc2e1e4b7f3df031c4e6ba5817662
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-12 18:30:18 +0100

    Added Marina model and client class for BookMarinas web service

---
+++ BalticMarinasClient/Models/Marina.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BalticMarinasClient.Models
+{
+    public class Marina
+    {
+        [JsonProperty("MarinaId")]
+        public int MarinaId { get; set; }
+
+        [JsonProperty("MarinaName")]
+        public string MarinaName { get; set; }
+
+        [JsonProperty("Phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        [JsonProperty("Depth")]
+        public double Depth { get; set; }
+
+        [JsonProperty("CityName")]
+        public string CityName { get; set; }
+
+        [JsonProperty("Country")]
+        public string Country { get; set; }
+
+        [JsonProperty("ZipCodeNumber")]
+        public string ZipCodeNumber { get; set; }
+
+        [JsonProperty("TotalBerths")]
+        public int TotalBerths { get; set; }
+
+        [JsonProperty("IsToilet")]
+        public int IsToilet { get; set; }
+
+        [JsonProperty("IsShower")]
+        public int IsShower { get; set; }
+
+        [JsonProperty("IsInternet")]
+        public int IsInternet { get; set; }
+    }
+}
