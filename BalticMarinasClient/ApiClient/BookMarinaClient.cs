5abd9afc57db38f35a342a3457554e9add8c2092
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-13 00:47:10 +0100

    Added BerthController and views for it

--- BalticMarinasClient/ApiClient/BookMarinaClient.cs
+++ BalticMarinasClient/ApiClient/BookMarinaClient.cs
@@ -76,5 +76,97 @@ namespace BalticMarinasClient.ApiClient
 
             return marinaResult;
         }
+
+        public async Task<ObservableCollection<Berth>> GetAllBerths()
+        {
+            var result = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.marinaServiceBase);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var urlAddress = berthServiceBase;
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(urlAddress).ConfigureAwait(continueOnCapturedContext: false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = await response.Content.ReadAsStringAsync();
+                    }
+
+                    var list = JsonConvert.DeserializeObject<ObservableCollection<Berth>>(result);
+                    return list;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Error: {e.StackTrace}");
+                }
+            }
+        }
+
+        public async Task<ObservableCollection<Berth>> GetBerthsByMarinaId(int? marinaId)
+        {
+            var result = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.marinaServiceBase);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var urlAddress = berthServiceBase + "/" + marinaId;
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(urlAddress).ConfigureAwait(continueOnCapturedContext: false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = await response.Content.ReadAsStringAsync();
+                    }
+
+                    var list = JsonConvert.DeserializeObject<ObservableCollection<Berth>>(result);
+                    return list;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Error: {e.StackTrace}");
+                }
+            }
+        }
+
+        public async Task<Berth> GetBerthByMarinaIdAndBerthId(int? marinaId, int? berthId)
+        {
+            var result = string.Empty;
+            var berthResult = new Berth();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.berthServiceBase);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var urlAddress = berthServiceBase + "/" + marinaId + "/" + berthId;
+
+                HttpResponseMessage response = await client.GetAsync(urlAddress).ConfigureAwait(continueOnCapturedContext: false);
+                if (response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
+
+                try
+                {
+                    berthResult = JsonConvert.DeserializeObject<Berth>(result);
+                }
+                catch (Exception e)
+                {
+                    //this.logger.Error($"Error in GetUserByEmail - {e}");
+                }
+            }
+
+            return berthResult;
+        }
     }
 }
--- BalticMarinasClient/Controllers/BerthController.cs
+++ BalticMarinasClient/Controllers/BerthController.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BalticMarinasClient.ApiClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BalticMarinasClient.Controllers
 {
     public class BerthController : Controller
     {
-        public IActionResult Index()
+        BookMarinaClient bookMarinaClient = new BookMarinaClient();
+
+        public IActionResult Index(int? id)
         {
+            var berths = bookMarinaClient.GetBerthsByMarinaId(id).Result;
+            ViewBag.BerthsList = berths;
+            return View();
+        }
+
+        public IActionResult Details(int? marinaId, int? berthId)
+        {
+            var berths = bookMarinaClient.GetBerthByMarinaIdAndBerthId(marinaId, berthId).Result;
+            ViewBag.BerthsList = berths;
             return View();
         }
     }
