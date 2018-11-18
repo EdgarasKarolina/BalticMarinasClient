ba87015bb59746abd614229e3d5dfb08a97368f1
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-18 23:29:04 +0100

    Added EmailClient.cs

---
+++ BalticMarinasClient/ApiClient/EmailClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BalticMarinasClient.ApiClient
+{
+    public class EmailClient
+    {
+        private string serviceBase = "https://localhost:44392/api/email";
+        
+    }
+}
