705d8c922ae6d17138122810b7f1979226e74f05
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-19 10:38:46 +0100

    Added Email model and updated EmailClient class

--- BalticMarinasClient/ApiClient/EmailClient.cs
+++ BalticMarinasClient/ApiClient/EmailClient.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BalticMarinasClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace BalticMarinasClient.ApiClient
 {
     public class EmailClient
     {
-        private string serviceBase = "https://localhost:44392/api/email";
-        
+        private string emailServiceBase = "https://localhost:44392/api/email";
+
+        public async void CreateReservation(string emailBody, string receiver)
+        {
+            Email email = new Email();
+            email.EmailBody = emailBody;
+            email.Receiver = receiver;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.emailServiceBase);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var json = JsonConvert.SerializeObject(email);
+
+                HttpResponseMessage response = await client.PostAsync(emailServiceBase + "/" + emailBody + "/" + receiver, new StringContent(json, Encoding.UTF8, "application/json"));
+                response.EnsureSuccessStatusCode();
+            }
+        }
     }
 }
---
+++ BalticMarinasClient/Models/Email.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BalticMarinasClient.Models
+{
+    public class Email
+    {
+        [JsonProperty("EmailBody")]
+        public string EmailBody { get; set; }
+
+        [JsonProperty("Receiver")]
+        public string Receiver { get; set; }
+    }
+}
