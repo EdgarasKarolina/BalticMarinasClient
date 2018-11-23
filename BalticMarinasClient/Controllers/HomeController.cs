99db72eb93ab96a8edbd8811c5ecba802bcedd15
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-23 18:36:17 +0100

    Removed not used Contacts page with controller method

--- BalticMarinasClient/Controllers/HomeController.cs
+++ BalticMarinasClient/Controllers/HomeController.cs
@@ -23,13 +23,6 @@ namespace BalticMarinasClient.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
