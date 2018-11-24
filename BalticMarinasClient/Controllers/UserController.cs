8cb5c501aa65642b228f69f0f301b03f6c2cd9d7
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-24 22:10:48 +0100

    Added RegistrationForm view

--- BalticMarinasClient/Controllers/UserController.cs
+++ BalticMarinasClient/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BalticMarinasClient.ApiClient;
+using BalticMarinasClient.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BalticMarinasClient.Controllers
@@ -6,9 +7,12 @@ namespace BalticMarinasClient.Controllers
     public class UserController : Controller
     {
         UserClient userClient = new UserClient();
-        public IActionResult Index()
+
+        public IActionResult CreateUser(string userName, string userPassword, string firstName, string lastName, string email, string phoneNumber, string country, int isAdmin)
         {
-            return View();
+            User user = new User() { UserName = userName, UserPassword = userPassword, FirstName = firstName, LastName = lastName, Email = email, PhoneNumber = phoneNumber, Country = country, IsAdmin = isAdmin};
+            userClient.CreateUser(user);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
